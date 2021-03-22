1fc3f070263e17bae6f8e65a893615dd67144e36
Rosstail <rosstail_zegeek@hotmail.fr>
2021-03-22 13:48:56 +0100

    Added proto doc

--- welcome.py
+++ welcome.py
@@ -5,15 +5,29 @@ Copyright Lololol
 
 
 def input_checker():
+    """
+    Ask an input to user and return it
+    :return: string_input
+    """
     string_input = input("Entrez votre chaine : ")
     return string_input
 
 
 def string_to_array(string):
+    """
+        Convert string parameter to array and returns it
+    :param string:
+    :return: list of string parameter
+    """
     return list(string)
 
 
 def string_to_ascii(string):
+    """
+        Convert string parameter to int array and returns it
+    :param string:
+    :return: array of int
+    """
     array = []
     for char in string:
         print(char)
@@ -22,10 +36,28 @@ def string_to_ascii(string):
 
 
 def num_to_bin(number_list):
+    """
+        convert array of int paramater to array of string (as binary) and returns it
+    :param number_list:
+    :return:
+    """
     array = []
     for number in number_list:
         array.append(bin(number)[2:].zfill(8))
     return array
+
+
+def string_split_interval(string):
+    """
+        Convert string parameter to array of string then returns it.
+        Each string int array is 6 chars long.
+    :param string:
+    :return:
+    """
+    split_strings = []
+    n = 6
+    for index in range(0, len(string), n):
+        split_strings.append(string[index: index + n])
 
 
 def main():

ae2eff42532112b440078727723c694272c9a234
Rosstail <rosstail_zegeek@hotmail.fr>
2021-03-22 13:33:10 +0100

    ex done 1 -> 6

--- welcome.py
+++ welcome.py
@@ -6,17 +6,44 @@ Copyright Lololol
 import base64
 
 
-def main():
+def input_checker():
     string_input = input("Entrez votre chaine : ")
+    return string_input
 
-    test_array = list(string_input)
-    test_ascii = []
+
+def string_to_array(string):
+    return list(string)
+
+
+def string_to_ascii(string):
+    array = []
+    for char in string:
+        print(char)
+        array.append(ord(char))
+    return array
+
+
+def num_to_bin(number_list):
+    array = []
+    for number in number_list:
+        array.append(bin(number)[2:].zfill(8))
+    return array
+
+
+def main():
+    string_input = input_checker()
+
+    test_array = string_to_array(string_input)
     print(test_array)
-    print('')
 
-    for char in test_array:
-        test_ascii.append(ord(char))
-    print(test_ascii)
+    test_array = string_to_ascii(test_array)
+    print(test_array)
+
+    test_array = num_to_bin(test_array)
+    print(test_array)
+
+    test_array = ''.join(test_array)
+    print(test_array)
 
 
 if __name__ == '__main__':
